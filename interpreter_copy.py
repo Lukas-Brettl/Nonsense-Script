26a2c7a0ba590ee905be41752d338232e2d72be5
Lukas-Brettl <153751463+Lukas-Brettl@users.noreply.github.com>
2024-06-17 15:33:43 +0200

    Update interpreter_copy.py

--- interpreter_copy.py
+++ interpreter_copy.py
@@ -18,26 +18,69 @@ def lexer(contents):
     sentence = []
     word = ""
     for line in lines:
+        object_["line" + str(numLine)] = {}
         for letter in line:
-            if letter in ["(", ")", '"', "'"] and word:
+            if letter in ["(", ")", '"', "'", ","] and word:
                 if word in keywords:
-                    object_[word] = keywordsObject_[word]
+                    object_["line" + str(numLine)][word] = keywordsObject_[word]
                     sentence.append(word)
+
                 else:
-                    object_[word] = {"type": "undefinited",}
+                    object_["line" + str(numLine)][word] = {"type": "undefinited",}
                     sentence.append(word)
                 word = letter
-                object_[word] = {"type": "undefinited",}
+                object_["line" + str(numLine)][word] = {"type": "undefinited",}
                 sentence.append(word)
                 word = ""
-            elif letter in ["(", ")", '"', "'"] and not word:
+            elif letter in ["(", ")", '"', "'", ","] and not word:
                 word += letter
-                object_[word] = {"type": "undefinited",}
+                object_["line" + str(numLine)][word] = {"type": "undefinited",}
                 sentence.append(word)
                 word=""
             else:
                 word += letter
+        
         print(sentence)
+
+        startSting = ""
+        endString = ""
+        if "'" in sentence:
+            for i, element in enumerate(sentence):
+                if element == "'" and not startSting:
+                    startSting = i
+                elif element == "'" and startSting:
+                    endString = i
+
+        elif '"' in sentence:
+            for i, element in enumerate(sentence):
+                if element == '"' and not startSting:
+                    startSting = i
+                elif element == '"' and startSting:
+                    endString = i
+        
+        if startSting and endString:
+            for i in range(startSting + 1, endString):
+                object_["line" + str(numLine)][sentence[i]]["type"] = "string"
+        
+        for i in sentence:
+            if object_["line" + str(numLine)][i]["type"] == "buildInFunction":
+                functionIndex = sentence.index(i)
+                editSentence = sentence[functionIndex + 1:]
+
+                leftCall = editSentence.index("(")
+                rightCall = editSentence.index(")")
+
+                valueRange = editSentence[leftCall + 1:rightCall]
+                print(valueRange)
+                callFunction = False
+                for value in valueRange:
+                    if value in [" ", ",", "'", '"']:
+                        continue        
+                    else:
+                        printToConsole(value)
+                
+                
+        numLine += 1
         sentence.clear()
     print(object_)
 def parse(file):
